ff6b24c9596d1727d72a06a4601d98b9430641f3
Janushas <78543941+Janushas@users.noreply.github.com>
2022-04-12 22:47:58 +0300

    Changed how the app gets it's current location

--- app/src/main/java/com/example/ejunasapp/Tools.java
+++ app/src/main/java/com/example/ejunasapp/Tools.java
@@ -3,7 +3,7 @@ package com.example.ejunasapp;
 import android.Manifest;
 
 public class Tools {
-    public static String RestURL = "http://10.0.2.2:8000/";
-
+    //public static String RestURL = "http://10.0.2.2:8000/";
+    public static String RestURL = "http://192.168.1.68:8000/";
 
 }
